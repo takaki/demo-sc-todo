ef2c705b18d1840ad11e7ce19a1e570f69df16e8
TANIGUCHI Takaki <takaki@asis.media-as.org>
2023-12-11 19:33:03 +0900

    tailwind css 2

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main>
       <App />
+      <hr />
+      <h1 className="bg-sky-50 text-3xl font-bold underline">Hello tailwindcss</h1>
+      <h1 className="local-test-class">Hello tailwindcss</h1>
     </main>
   )
 }
