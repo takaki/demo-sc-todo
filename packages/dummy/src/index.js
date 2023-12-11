2e02f5899908e06982aa6813550976c061f62ffc
TANIGUCHI Takaki <takaki@asis.media-as.org>
2023-12-11 22:55:06 +0900

    common tsconfig

---
+++ packages/dummy/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SAMPLE_DUMMY_2 = exports.SAMPLE_DUMMY = void 0;
+exports.SAMPLE_DUMMY = 1;
+exports.SAMPLE_DUMMY_2 = 2;
